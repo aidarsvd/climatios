f3a95737cef3263056d115960030ed51dad1c49d
Aidar <achakiev2003@gmail.com>
2021-01-16 21:25:33 +0600

    Initial Commit

---
+++ self-study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  self-study
+//
+//  Created by Айдар Чакиев on 16/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
